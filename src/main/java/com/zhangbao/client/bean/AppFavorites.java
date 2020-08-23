a82e7008bd698dd8f81649b45c1717800f99dbe1
zhangbao <zhangbaohpu@163.com>
2020-08-23 23:37:05 +0800

    Initial commit

---
+++ src/main/java/com/zhangbao/client/bean/AppFavorites.java
@@ -0,0 +1,16 @@
+package com.zhangbao.client.bean;
+
+import lombok.Data;
+
+/**
+ * 收藏
+ */
+@Data
+public class AppFavorites {
+
+    private int id;//主键
+    private int course_id;//商品id
+    private int userid;//用户ID
+    private String add_time;//创建时间
+
+}
